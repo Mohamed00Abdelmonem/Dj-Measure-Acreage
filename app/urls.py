1f994655973b395d00c7cc003418591f95b08d3b
unknown <mmohamedabdelm@gmail.com>
2023-08-24 13:22:40 +0300

    updating in my project in my frontend and others

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import Area_List
+from .views import Area_List, Land_Detail
 
 
 urlpatterns = [
 
-  path('', Area_List.as_view(), name='list_land')
+  path('', Area_List.as_view(), name='list_land'),
+  path('land_detail/<int:pk>', Land_Detail.as_view(), name='land_detail')
 
 
 ]

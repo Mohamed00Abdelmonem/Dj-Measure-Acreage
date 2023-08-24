70d52a81c03be3eb7081e74bf2e708f6a987da35
unknown <mmohamedabdelm@gmail.com>
2023-08-24 07:33:24 +0300

    add admin.site.regitster in admin.py for show in admin panel

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Data_Land, Data_For_Author
 # Register your models here.
+
+admin.site.register(Data_For_Author)
+admin.site.register(Data_Land)
